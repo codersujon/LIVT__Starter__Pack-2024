6750b175325c82bcae0490e71c78c6a5df6ee688
unknown <csesujon155@gmail.com>
2024-08-20 10:49:22 -0700

    Ready Starter Pack

---
+++ resources/js/Pages/App.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div class="container mx-auto text-center">
+        <h1 class="text-5xl py-6 text-black-500">Laravel Inertia Vue Tailwind Starter Pack</h1>
+        <p class="text-4xl">2024</p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
